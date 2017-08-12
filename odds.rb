d52e69052375b39f98316ec14704f8a6c185a45f
Marlon O'Neil <hello@marlononeil.com>
2017-08-12 15:17:06 -0400

    exercise: odds

---
+++ odds.rb
@@ -0,0 +1,59 @@
+sides_of_the_dice = *(1..6)
+number_of_dice = 2
+count = 0
+
+#Create the hash necessary
+totals = []
+permutations = {}
+
+#Populate the hash with numbers 2 to 12
+(2..12).each do |number|
+  permutations[number] = 0
+end
+
+puts "The permutations before calculation"
+
+#Calculate two dice roll totals if permutations are rolld once
+sides_of_the_dice.repeated_permutation(number_of_dice) do |perm|
+  totals << perm.sum
+  count = count + 1
+end
+
+#Keep track of how many times the total rolls show up in the hash
+totals.each do |each_total|
+  if permutations.has_key?(each_total)
+    permutations[each_total] += 1
+  end
+end
+
+
+#Show results
+permutations.each do |key, value|
+  one_percent = (100/36.to_f)
+  percent = value * one_percent
+  puts "The odds of #{key} show up is #{percent.round}%"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+# permutations = {}
+#
+# (2..12).each { |number| permutations[number] = 0 }
+#
+# (1..6).each do |die1|
+#   (1..6).each do |die2|
+#     permutations[die1 + die2] += 1
+#   end
+# end
+#
+# permutations.each { |key, value| puts "#{key} occurs #{value} times" }
